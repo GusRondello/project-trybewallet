b6d4f94f999b383bdfc24c624d28e4ecf3717d02
Gustavo Rondello <gustavorondello99@outlook.com>
2022-04-10 11:27:03 -0300

    Requisito 7 completo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Form from '../Components/Form';
 import Header from '../Components/Header';
+import Table from '../Components/Table';
 
 class Wallet extends Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends Component {
       <div>
         <Header />
         <Form />
+        <Table />
       </div>
     );
   }
